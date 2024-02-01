68ae785c2e1247c0b9898a037e95baa55f8064ae
Jiyao17 <jiyao.liu@outlook.com>
2024-02-01 11:12:24 -0500

    update

--- src/physical/network/types.py
+++ src/physical/network/types.py
@@ -1,12 +1,12 @@
 
 
-from typing import NewType
+from typing import NewType, Tuple, List
 
 # types defined for networks
 NodeID = NewType('NodeID', int)
 # KeyID = NewType('KeyID', int)
-NodePair = NewType('NodePair', tuple[NodeID, NodeID])
-EdgeTuple = NewType('EdgeTuple', tuple[NodeID, NodeID])
+NodePair = NewType('NodePair', Tuple[NodeID, NodeID])
+EdgeTuple = NewType('EdgeTuple', Tuple[NodeID, NodeID])
 # MultiEdgeTuple = NewType('MultiEdgeTuple', tuple[NodeID, NodeID, KeyID])
-StaticPath = NewType('StaticPath', tuple[EdgeTuple])
-Path = NewType('Path', list[EdgeTuple])
+StaticPath = NewType('StaticPath', Tuple[EdgeTuple])
+Path = NewType('Path', List[EdgeTuple])
--- src/physical/quantum/__init__.py
+++ src/physical/quantum/__init__.py
@@ -9,11 +9,11 @@ from .types import *
 # perfect hardware
 HWP = HWParam((1, 1, 1, 1))
 # noisy hardware, high accuracy
-HWH = HWParam((0.999, 0.999, 0.999, 0.995))
+HWH = HWParam((0.99999, 0.99999, 0.99999, 0.9999))
 # noisy hardware, medium accuracy
-HWM = HWParam((0.995, 0.995, 0.995, 0.99))
+HWM = HWParam((0.9999, 0.9999, 0.9999, 0.999))
 # noisy hardware, low accuracy
-HWL = HWParam((0.99, 0.99, 0.99, 0.975))
+HWL = HWParam((0.999, 0.999, 0.99, 0.99))
 
 # Dephased operation, perfect
 GDP = Gate(EntType.DEPHASED, HWP)
